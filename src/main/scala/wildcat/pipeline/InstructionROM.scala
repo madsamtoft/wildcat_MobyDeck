b564eadcb55b357e1a63a342a5aa7b6058b7596c
Dejan <deja0079@outlook.dk>
2025-03-12 16:06:48 +0100

    Working registers for IM

--- src/main/scala/wildcat/pipeline/InstructionROM.scala
+++ src/main/scala/wildcat/pipeline/InstructionROM.scala
@@ -10,7 +10,12 @@ class InstructionROM(code: Array[Int]) extends Module {
 
   val addrReg = RegInit(0.U(32.W))
   addrReg := io.address
-  val instructions = VecInit(code.toIndexedSeq.map(_.S(32.W).asUInt))
+
+  val codeSeq = code.toIndexedSeq.map(_.S(32.W).asUInt)
+  val codeSeqAppend = IndexedSeq.fill(512 - codeSeq.length)(0.U(32.W))
+  val codeFinal = codeSeq ++ codeSeqAppend
+
+  val instructions = RegInit(VecInit(codeFinal))
   io.data := instructions(addrReg(31, 2))
   // for checking two failing tests
   val toggle = RegInit(false.B)
