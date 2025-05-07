99e96b6026bb252453800b3d45d392ed2efe2dff
Dejan <deja0079@outlook.dk>
2025-05-07 13:11:11 +0200

    Changed VGA test

--- src/test/scala/wildcat/ownTests/TestVgaSimple.scala
+++ src/test/scala/wildcat/ownTests/TestVgaSimple.scala
@@ -10,9 +10,11 @@ class TestVgaSimple extends AnyFlatSpec with ChiselScalatestTester {
     test(new WildcatTop("../SoC_project/out/vga_simple.out")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.clock.setTimeout(0)
       dut.io.sw.poke("b001000".U)
-      dut.clock.step(100)
-      dut.io.sw.poke("b110000".U)
-      dut.clock.step(10000)
+      dut.clock.step(6000)
+      dut.io.sw.poke("b000001".U)
+      dut.clock.step(20000)
+      dut.io.sw.poke("b000000".U)
+      dut.clock.step(20000)
     }
   }
 }
