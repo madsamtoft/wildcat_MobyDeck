735e418bdc6bfdf92bfc2742f08f200c37a3bf29
Dejan <deja0079@outlook.dk>
2025-05-08 09:50:48 +0200

    Changed TestVgaSimple to test two different files

--- src/test/scala/wildcat/ownTests/TestVgaSimple.scala
+++ src/test/scala/wildcat/ownTests/TestVgaSimple.scala
@@ -11,12 +11,23 @@ class TestVgaSimple extends AnyFlatSpec with ChiselScalatestTester {
   "_VgaSimple test" should "pass" in {
     test(new WildcatTop("../SoC_project/out/vga_simple.out")).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation, VerilatorFlags(Seq("-CFLAGS", "-std=c++14")))) { dut =>
       dut.clock.setTimeout(0)
+      dut.clock.step(100)
       dut.io.sw.poke("b001000".U)
       dut.clock.step(6000)
-      dut.io.sw.poke("b000001".U)
-      dut.clock.step(20000)
-      dut.io.sw.poke("b000000".U)
-      dut.clock.step(20000)
+
+//      dut.io.sw.poke("b000001".U)
+      dut.clock.step(100000)
+    }
+  }
+  "_VgaSimple test2" should "pass" in {
+    test(new WildcatTop("../SoC_project/out/vga_simple2.out")).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation, VerilatorFlags(Seq("-CFLAGS", "-std=c++14")))) { dut =>
+      dut.clock.setTimeout(0)
+      dut.clock.step(100)
+      dut.io.sw.poke("b001000".U)
+      dut.clock.step(6000)
+
+//      dut.io.sw.poke("b000001".U)
+      dut.clock.step(100000)
     }
   }
 }
