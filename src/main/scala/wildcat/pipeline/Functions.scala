14f391e6c16af3e509105ea6a8baf31bcffcb483
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-06-19 17:14:16 +0200

    Only initializing stack pointer to x7FF0

--- src/main/scala/wildcat/pipeline/Functions.scala
+++ src/main/scala/wildcat/pipeline/Functions.scala
@@ -191,14 +191,26 @@ object Functions {
 
     if (useMem) {
       val regs = SyncReadMem(32, UInt(32.W), SyncReadMem.WriteFirst)
-      regs.write(2.U, "x7FF0".U) // init sp
+      //regs.write(2.U, "x7FF0".U) // init sp
       val debugRegs = RegInit(VecInit(Seq.fill(32)(0.U(32.W)))) // only for debugging, not used in synthesis
       val rs1Val = Mux(RegNext(rs1) === 0.U, 0.U, regs.read(rs1))
       val rs2Val = Mux(RegNext(rs2) === 0.U, 0.U, regs.read(rs2))
+
+      val spInitialized = RegInit(false.B)
+
+      when(!spInitialized) {
+        regs.write(2.U, "x7FF0".U)
+        spInitialized := true.B
+      }.elsewhen(wrEna && rd =/= 0.U) {
+        regs.write(rd, wrData)
+      }
+
+      /*
       when(wrEna && rd =/= 0.U) {
         regs.write(rd, wrData)
         debugRegs(rd) := wrData
       }
+      */
       (rs1Val, rs2Val, debugRegs)
     } else {
       // non need for forwarding as read address is delayed
